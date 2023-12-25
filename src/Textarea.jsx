f9bb176336b04d9f3f06d15922214f2e19829a89
Will Price <pricew86@gmail.com>
2023-12-25 11:07:59 +0800

    validation and start stats

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,3 +1,41 @@
+import { useState } from 'react';
+import Warning from './Warning';
+
 export default function Textarea() {
-  return <textarea className='textarea' />;
+  const [text, setText] = useState('');
+  const [warningText, setWarningText] = useState('');
+
+  // useEffect(() => {
+  //   if (warningText) {
+  //     setTimeout(() => {
+  //       setWarningText('');
+  //     }, 2000);
+  //   }
+  // }, [warningText]);
+
+  const handleChange = (e) => {
+    let newText = e.target.value;
+
+    if (newText.includes('<script>')) {
+      setWarningText('No script tag allowed!');
+      newText = newText.replace('<script>', '');
+    } else if (newText.includes('@')) {
+      setWarningText('No @ allowed!');
+      newText = newText.replace('@', '');
+    } else {
+      setWarningText('');
+    }
+    setText(newText);
+  };
+  return (
+    <div className='textarea'>
+      <textarea
+        value={text}
+        placeholder='Enter your text'
+        spellCheck='false'
+        onChange={handleChange}
+      />
+      <Warning warningText={warningText} />
+    </div>
+  );
 }
